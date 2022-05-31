9c9e344c8e1ee3af67d1384f9c2421b3ade91c40
gabrila bermejo <gabrielabermejo@uninorte.edu.co>
2022-05-30 20:04:41 -0500

    17 commit

--- froms/informacion.py
+++ froms/informacion.py
@@ -15,38 +15,38 @@ class Info:
 
         self.miFrame2=Frame(self.ventanita2, width=500, height=500)
         self.miFrame2.pack()
-        self.miFrame2.config(bg='black')
-        self.miLabel2=Label(self.miFrame2, text="===============RESULTADOS===============", font=("The Monotype Corporation", 16), fg='white')
+        self.miFrame2.config(bg='white')
+        self.miLabel2=Label(self.miFrame2, text="===============RESULTADOS===============", font=("The Monotype Corporation", 16), fg='black')
         self.miLabel2.place(x=1, y=14)
-        self.miLabel2.config(bg='black')
+        self.miLabel2.config(bg='white')
 
         self.miFrame3=Frame(self.miFrame2, width=500, height=15)
         self.miFrame3.place(x=0,y=0)
-        self.miFrame3.config(bg='lime')
+        self.miFrame3.config(bg='paleturquoise')
 
         self.miFrame4=Frame(self.miFrame2, width=500, height=15)
         self.miFrame4.place(x=0,y=40)
-        self.miFrame4.config(bg='lime')
+        self.miFrame4.config(bg='paleturquoise')
 
-        self.miLabel2=Label(self.miFrame2, text="___________________________________________", font=("The Monotype Corporation", 16), fg='lime')
+        self.miLabel2=Label(self.miFrame2, text="___________________________________________", font=("The Monotype Corporation", 16), fg='paleturquoise')
         self.miLabel2.place(x=1, y=86)
-        self.miLabel2.config(bg='black')
+        self.miLabel2.config(bg='white')
 
-        pesoLabel2=Label(self.miFrame2, text="Peso: "+str(persona.peso),font=14, padx=-12,fg='white', bg='black')
+        pesoLabel2=Label(self.miFrame2, text="Peso: "+str(persona.peso),font=14, padx=-12,fg='black', bg='white')
         pesoLabel2.place(x=220, y=85)
 
-        self.miLabel2=Label(self.miFrame2, text="___________________________________________", font=("The Monotype Corporation", 16), fg='lime')
+        self.miLabel2=Label(self.miFrame2, text="___________________________________________", font=("The Monotype Corporation", 16), fg='paleturquoise')
         self.miLabel2.place(x=1, y=116)
-        self.miLabel2.config(bg='black')
+        self.miLabel2.config(bg='white')
         
-        alturaLabel2=Label(self.miFrame2, text="Altura: "+str(persona.altura),font=14, padx=-12, fg='white', bg='black')
+        alturaLabel2=Label(self.miFrame2, text="Altura: "+str(persona.altura),font=14, padx=-12, fg='black', bg='white')
         alturaLabel2.place(x=220, y=115)
 
-        self.miLabel2=Label(self.miFrame2, text="___________________________________________", font=("The Monotype Corporation", 16), fg='lime')
+        self.miLabel2=Label(self.miFrame2, text="___________________________________________", font=("The Monotype Corporation", 16), fg='paleturquoise')
         self.miLabel2.place(x=1, y=146)
-        self.miLabel2.config(bg='black')
+        self.miLabel2.config(bg='white')
 
-        edadLabel2=Label(self.miFrame2, text="Edad: "+str(persona.edad), font=14,padx=-12, bg='black', fg='white')
+        edadLabel2=Label(self.miFrame2, text="Edad: "+str(persona.edad), font=14,padx=-12, bg='white', fg='black')
         edadLabel2.place(x=220, y=145)
 
 
@@ -77,22 +77,22 @@ class Info:
         print(Mensaje.get())
         print(str(IMC))
 
-        IMCLabel2=Label(self.miFrame2, text="Su IMC es de: "+str(IMC), padx=-12, fg='white', bg='black', font=("Arial", 13))
+        IMCLabel2=Label(self.miFrame2, text="Su IMC es de: "+str(IMC), padx=-12, fg='black', bg='white', font=("Arial", 13))
         IMCLabel2.place(x=135, y=200)
 
-        estadoLabel2=Label(self.miFrame2, text="Estado: "+Mensaje.get(), padx=-12, fg='white', bg='black')
-        estadoLabel2.place(x=150, y=255)
+        estadoLabel2=Label(self.miFrame2, text="Estado: "+Mensaje.get(), padx=-12, fg='black', bg='white', font=("Arial", 13))
+        estadoLabel2.place(x=135, y=235)
 
 
-        recomendacionesLabel2=Label(self.miFrame2, text="Recomendaciones: "+recomendacion.get(), padx=-12, bg='black', fg='white')
-        recomendacionesLabel2.place(x=150, y=280)
+        recomendacionesLabel2=Label(self.miFrame2, text="Recomendaciones: "+recomendacion.get(), padx=-12, bg='white', fg='black', font=("Arial", 10))
+        recomendacionesLabel2.place(x=135, y=265)
 
 
         def codigoBoton2():
     
             Recetas()
 
-        botonInstru=Button(self.ventanita2, text="RECETAS", command=codigoBoton2, bg='white', fg='black')
+        botonInstru=Button(self.ventanita2, text="RECETAS", command=codigoBoton2, bg='paleturquoise', fg='black')
         botonInstru.place(x=15, y=380)
 
         g=persona.altura-100
@@ -102,7 +102,7 @@ class Info:
         else:
             Mensaje.set('Su peso ideal es de: '+str(g))
             
-        gLabel2=Label(self.miFrame2, text="Acerca de su peso ideal: "+Mensaje.get(),  font=("Arial", 11, BOLD, ITALIC), fg='white', bg='black')
+        gLabel2=Label(self.miFrame2, text="Acerca de su peso ideal: "+Mensaje.get(),  font=("Arial", 11, BOLD, ITALIC), fg='black', bg='white')
         gLabel2.place(x=45, y=350)
 
 
